a93daf849c9c574ff0a88e45d777bb14f20a00ef
herokingsley <410638015@qq.com>
2014-04-27 11:59:30 +0800

    Initial Commit

---
+++ MoneyManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MoneyManager
+//
+//  Created by HeroKingsley on 4/27/14.
+//  Copyright (c) 2014 王智锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MoneyManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MoneyManager
+//
+//  Created by HeroKingsley on 4/27/14.
+//  Copyright (c) 2014 王智锐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
